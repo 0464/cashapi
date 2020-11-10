689543f2d694754e2691eed39747713c202159ee
GDJ29 <GDJ29@DESKTOP-CPG461F>
2020-11-10 18:15:36 +0900

    CashListExcel fix

--- cashapi/src/main/java/kr/co/gdu/cashapi/view/CashListExcel.java
+++ cashapi/src/main/java/kr/co/gdu/cashapi/view/CashListExcel.java
@@ -5,6 +5,8 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.springframework.stereotype.Component;
@@ -21,9 +23,12 @@ public class CashListExcel extends AbstractXlsView {
 		// 응답 body의 내용을 파일로 전송(cash_list.xls)
 		response.setHeader("Content-Disposition", "attachment; filename=\"cash_list.xls\"");
 		// 매개변수로 입력받은 Workbook(엑셀파일객체)에 첫번째 Sheet를 접근
-		Sheet sheet1 = workbook.createSheet("sheet1");
-		sheet1.createRow(0).createCell(0).setCellValue("이름");
-		sheet1.createRow(0).createCell(1).setCellValue("구디아카데미");
+		Sheet sheet = workbook.createSheet("sheet1");
+		Row row0 = sheet.createRow(0);
+		Cell cell0 = row0.createCell(0);
+		cell0.setCellValue("이름");
+		Cell cell1 = row0.createCell(1);
+		cell1.setCellValue("Goodee");
 	}
 	
 }
