7693de766a6fb68bee8d6462dd72ae84019e663e
GDJ29 <GDJ29@DESKTOP-CPG461F>
2020-11-10 18:08:24 +0900

    pom.xml poi 외부라이브러리 추가, view 패키지 추가, CashListExcel 클래스 생성, excel 다운 테스트

---
+++ cashapi/src/main/java/kr/co/gdu/cashapi/view/CashListExcel.java
@@ -0,0 +1,29 @@
+package kr.co.gdu.cashapi.view;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.view.document.AbstractXlsView;
+
+@Component("cashListExcel") // CashListExcel cashListExcel = new CashListExcel();
+public class CashListExcel extends AbstractXlsView {
+
+	@Override
+	protected void buildExcelDocument(Map<String, Object> model, Workbook workbook, HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		// 리스트를 가지고와 엑셀파일 생성
+		
+		// 응답 body의 내용을 파일로 전송(cash_list.xls)
+		response.setHeader("Content-Disposition", "attachment; filename=\"cash_list.xls\"");
+		// 매개변수로 입력받은 Workbook(엑셀파일객체)에 첫번째 Sheet를 접근
+		Sheet sheet1 = workbook.createSheet("sheet1");
+		sheet1.createRow(0).createCell(0).setCellValue("이름");
+		sheet1.createRow(0).createCell(1).setCellValue("구디아카데미");
+	}
+	
+}
